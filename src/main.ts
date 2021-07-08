0b22d010906fce36b4e14cf3aabb6834ea0663d9
xiaobai <xiaobai-world@foxmail.com>
2021-07-08 23:29:34 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { mount } from '@xiaobai-world/api'
+
+function render(selector: string) {
+ createApp(App).mount(selector)
+}
+
+mount(render).catch(() => {
+ render('#app')
+})
